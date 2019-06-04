a10b82c4ca71cb9f01eee7990b72f48b7f65f7cf
Nelson Calsolari Neto <ncalsolari@usp.br>
2019-06-04 11:59:59 -0300

    v1 update gl bn

--- Metodo_GL/sequencial/sem_bign/metodo-gl-seq.c
+++ Metodo_GL/sequencial/sem_bign/metodo-gl-seq.c
@@ -7,101 +7,79 @@
 #include <gmp.h>
 #include <time.h>
 
-//precisao
-	mpf_set_default_prec(pow(10,5));
-	
-//inicializando variaveis com valor 0
-
-	mpf_t pi;
-	mpf_t var_a;
-	mpf_t var_b;
-	mpf_t var_t;
-	mpf_t var_p;
-	
-	mpf_t var_a1;
-	mpf_t var_b1;
-	mpf_t var_t1;
-	mpf_t var_p1;
-	
-	
-	mpf_init(pi);
-	mpf_init(var_a);
-	mpf_init(var_b);
-	mpf_init(var_t);
-	mpf_init(var_p);
-	mpf_init(var_a1);
-	mpf_init(var_b1);
-	mpf_init(var_t1);
-	mpf_init(var_p1);
-	
-	
-// valores iniciais
-
-	mpf_init_set_d(var_a,1);
-	mpf_init_set_d(var_b,1/sqrt(2));
-	mpf_init_set_d(var_t,0.25);
-	mpf_init_set_d(var_p,1);
-	
-	
-	//iterador
-	
-	int i = 0;
-	
 
 void main(){
-	
-	
-	
-	
-	while(i<100){
-		
-			
-			//a1 = (a+b)/2
-		mpf_add(var_a1,var_a,var_b);
-		mpf_div_ui(var_a1,var_a1,2);
-		
-		
-		//b1 = sqrt(a*b)
-		mpf_mul_ui(var_b1,var_a,var_b);
-		mpf_sqrt(varb1,varb1);
-		
-		
-		//t1= t-p(a-a1)^2
-		mpf_sub(var_t1,var_a,var_a1);
-		mpf_pow_ui(var_t1,var_t1,2);
-		mpf_mul_ui(var_t1,var_p,var_t1);
-		mpf_sub(var_t1,var_t,var_t);
-		
-		
-		//p1 = 2*p
-		mpf_mul_ui(var_p1,2,var_p);
-		
-		
-		// pi == ((a1+b1)^2)/(4*t1)
-		mpf_add(pi,var_a1,var_b1);
-		mpf_pow_ui(pi,pi,2);
-		mpf_div_ui(pi,pi,4);
-		mpf_div_ui(pi,pi,var_t1);
-		
-		
-		//atualiza as variaveis
-		
-		mpf_set(var_a,var_a1);
-		mpf_set(var_b,var_b1);
-		mpf_set(var_t,var_t1);
-		mpf_set(var_p,var_p1);
-		
-		
-		i++;
-		
-		
-		//printa pi
-		gmp_printf("Valor de pi: %.6Ff\n",pi);
-		
-		
 
+	double pi;
+
+	double var_a;
+	double var_b;
+	double var_t;
+	double var_p;
+
+
+
+
+	double var_a1;
+	double var_b1;
+	double var_t1;
+	double var_p1;
+
+
+	var_a = 1;
+	var_b = (1/sqrt(2));
+	var_t = 0.25;
+	var_p = 1;
+
+	var_a1 = ((var_a + var_b)/2);
+	var_b1 = sqrt(var_a*var_b);
+	var_t1 = var_t - var_p*((var_a-var_a1)*(var_a-var_a1));
+	var_p1 = 2*var_p;
+
+	pi = ((var_a1 + var_b1)*(var_a1 + var_b1))/(4*var_t1);
+
+	int controle = 1;
+
+	printf("a %lf\n",var_a );
+	printf("b %lf\n",var_b );
+	printf("t %lf\n",var_t );
+	printf("p %lf\n",var_p );
+	printf("a1 %lf\n",var_a1 );
+	printf("b1 %lf\n",var_b1);
+	printf("t1 %lf\n",var_t1 );
+	printf("p1 %lf\n",var_p1 );
+
+
+
+
+
+
+
+	printf("entrei no while \n\n" );
+
+	while(controle<1000){
+		
+		pi = ((var_a1 + var_b1)*(var_a1 + var_b1))/(4*var_t1);
+
+		var_a = var_a1;
+		var_b = var_b1;
+		var_t = var_t1;
+		var_p = var_p1;
+		
+		
+		var_a1 = (var_a + var_b)/2;
+		var_b1 = sqrt(var_a*var_b);
+		var_t1 = var_t - var_p*((var_a-var_a1)*(var_a-var_a1));
+		var_p1 = 2*var_p;
+		
+		controle++;
+/*
+		if(controle%10 == 0){
+			printf("%d\n", controle );
+			printf("%.15lf\n", pi ); // trocar para mostrar apenas as 6 casas
+		}
+		*/
+		
 	}
-	
-	
-	
+	printf("%.15lf\n", pi );
 }
