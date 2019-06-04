6e1e2935fd999c10fb228bff4b76ab5409bd7511
ncalsolari <ncalsolari@usp.br>
2019-06-04 17:46:15 -0300

    v1 update metodo gl seq bn

--- Metodo_GL/sequencial/com_bign/metodo-gl-seq-bn.c
+++ Metodo_GL/sequencial/com_bign/metodo-gl-seq-bn.c
@@ -1,3 +1,9 @@
+/*
+Para compilação no linux, lembrar de incluir as bibliotecas, como descrito abaixo. 
+- gcc nome_programa.c -o nome_programa - lm -lgmp
+ */
+
+
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,7 +12,12 @@
 #include <gmp.h>
 #include <time.h>
 
-//precisao
+
+	
+
+void main(){
+
+	//precisao
 	mpf_set_default_prec(pow(10,5));
 	
 //inicializando variaveis com valor 0
@@ -46,41 +57,41 @@
 	
 	int i = 0;
 	
-
-void main(){
 	
-	
-	
-	
-	while(i<100){
+	while(i<1000000000){
 		
-			
+			printf("%d\n", 1000000000 - i );
 			//a1 = (a+b)/2
 		mpf_add(var_a1,var_a,var_b);
 		mpf_div_ui(var_a1,var_a1,2);
 		
 		
 		//b1 = sqrt(a*b)
-		mpf_mul_ui(var_b1,var_a,var_b);
-		mpf_sqrt(varb1,varb1);
+		mpf_mul(var_b1,var_a,var_b);
+		mpf_sqrt(var_b1,var_b1);
 		
 		
 		//t1= t-p(a-a1)^2
 		mpf_sub(var_t1,var_a,var_a1);
 		mpf_pow_ui(var_t1,var_t1,2);
-		mpf_mul_ui(var_t1,var_p,var_t1);
-		mpf_sub(var_t1,var_t,var_t);
+		mpf_mul(var_t1,var_p,var_t1);
+		mpf_sub(var_t1,var_t,var_t1);
 		
 		
 		//p1 = 2*p
-		mpf_mul_ui(var_p1,2,var_p);
+		mpf_add(var_p1,var_p,var_p);
 		
 		
 		// pi == ((a1+b1)^2)/(4*t1)
 		mpf_add(pi,var_a1,var_b1);
+
 		mpf_pow_ui(pi,pi,2);
+
 		mpf_div_ui(pi,pi,4);
-		mpf_div_ui(pi,pi,var_t1);
+
+	
+		mpf_div(pi,pi,var_t1);
+		
 		
 		
 		//atualiza as variaveis
@@ -91,6 +102,7 @@ void main(){
 		mpf_set(var_p,var_p1);
 		
 		
+	
 		i++;
 		
 		
@@ -98,6 +110,7 @@ void main(){
 		gmp_printf("Valor de pi: %.6Ff\n",pi);
 		
 		
+	
 
 	}
 	
