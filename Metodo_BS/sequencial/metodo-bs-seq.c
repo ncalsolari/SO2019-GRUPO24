ca712e610cdf9e2b9755b79354402954d31654d4
ncalsolari <ncalsolari@usp.br>
2019-06-21 16:29:07 -0300

    add metodo blackscholes

---
+++ Metodo_BS/sequencial/metodo-bs-seq.c
@@ -0,0 +1,60 @@
+/*
+Para compilação no linux, lembrar de incluir as bibliotecas, como descrito abaixo. 
+- gcc nome_programa.c -o nome_programa -lm -lgmp
+ */
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <gmp.h>
+#include <time.h>
+
+
+	int main(){
+
+
+
+		double exponencial1 = (r(-0.5*sigma*sigma));
+		double exponencial2 = (-1)*r*var_t
+
+
+		for(int i=0; i<M ; i++){
+
+
+
+			t= (s*pow(M_E,exponencial1)*var_t + sigma*sqrt(var_t)*nmero_aleatorio);
+
+			vetor[i]= pow(M_E,exponencial2)*fmax((t-E),0);
+
+
+
+
+
+
+
+
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
